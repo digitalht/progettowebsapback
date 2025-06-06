318a620171f25b7ff5ea1030e85b4f71f3ead443
H.T. Digital Team <61697667+digitalht@users.noreply.github.com>
2025-06-06 17:17:33 +0200

    Update serverorder.js

--- serverorder.js
+++ serverorder.js
@@ -1,4 +1,5 @@
-
+import dotenv from "dotenv";
+dotenv.config();
 
 // serverorder.js
 import express from "express";
@@ -7,7 +8,7 @@ import btoa from "btoa";
 //import fetch from 'node-fetch';
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
